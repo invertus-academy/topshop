385a03d3d8006ae380a04a81beebcca62c5733a0
RRR777 <ritaj777@gmail.com>
2019-02-28 12:39:43 +0200

    Find two hooks places for compare button

--- compareitems.php
+++ compareitems.php
@@ -18,17 +18,37 @@ class Compareitems extends Module {
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
+        if (!Configuration::get('MYMODULE_NAME')) {
+            $this->warning = $this->l('No name provided.');
+        }
     }
+
     public function install()
     {
-        return parent::install() && $this->registerHook('displayHeader');
+        return parent::install() &&
+            $this->registerHook('displayHeader') &&
+            $this->registerHook('displayProductPriceBlock') &&
+            $this->registerHook('displayProductListReviews') &&
+            Configuration::updateValue('MYMODULE_NAME', 'compareitems');
     }
+
     public function uninstall()
     {
         return parent::uninstall();
     }
+
     public function hookDisplayHeader()
     {
         echo "Viskas veikia";
+    }
+
+    public function hookDisplayProductPriceBlock()
+    {
+        echo "Compare";
+    }
+
+    public function hookDisplayProductListReviews()
+    {
+        echo "Review";
     }
 }
