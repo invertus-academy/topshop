2abc102e135c2fb8759c5c0626992c2909328415
RRR777 <ritaj777@gmail.com>
2019-03-05 19:20:41 +0200

    Added Admin Controllers

---
+++ controllers/admin/AdminCompareItemsConfigurationController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritaj
+ * Date: 3/5/2019
+ * Time: 6:29 PM
+ */
+
+class AdminCompareItemsConfigurationController extends ModuleAdminController
+{
+/*    public function __construct()
+    {
+        parent::__construct();
+        echo "Hello World!";
+    }*/
+
+}
---
+++ controllers/admin/AdminCompareItemsParentController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ritaj
+ * Date: 3/5/2019
+ * Time: 6:22 PM
+ */
+
+class AdminCompareItemsParentController extends ModuleAdminController
+{
+
+}
